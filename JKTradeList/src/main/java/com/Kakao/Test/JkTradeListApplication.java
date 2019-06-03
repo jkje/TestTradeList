3cfdfabaf04da0a6755816efddfc821074d721f1
jeongkwan2 <51329693+jeongkwan2@users.noreply.github.com>
2019-06-03 23:49:17 +0900

    Add files via upload

---
+++ JKTradeList/src/main/java/com/Kakao/Test/JkTradeListApplication.java
@@ -0,0 +1,13 @@
+package com.Kakao.Test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JkTradeListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JkTradeListApplication.class, args);
+	}
+
+}
